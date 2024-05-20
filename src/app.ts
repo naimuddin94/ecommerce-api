ba7291ea124e611f656e503a973a98c163aeba26
naimuddin94 <developernaimuddin@gmail.com>
2024-05-20 22:49:17 +0600

    handle global not found route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cors from 'cors';
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 
 const app: Application = express();
 
@@ -8,6 +8,11 @@ app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('server is running 🚀');
+});
+
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+  const error = new Error(`can't find ${req.originalUrl} on the server`);
+  next(error);
 });
 
 export default app;
