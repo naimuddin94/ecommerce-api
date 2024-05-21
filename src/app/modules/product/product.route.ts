6da1f2c9f8dc9662179e877ebd947f9ed80c3e7f
naimuddin94 <developernaimuddin@gmail.com>
2024-05-22 01:05:26 +0600

    update product.route.ts

--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -8,6 +8,9 @@ router
   .post(productController.createProduct)
   .get(productController.allProducts);
 
-router.route('/:productId').get(productController.singleProduct);
+router
+  .route('/:productId')
+  .get(productController.singleProduct)
+  .put(productController.updateProduct);
 
 export const productRouter = router;
