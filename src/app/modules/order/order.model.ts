d4cfa39687e455a324921eabf3add54074bd04c5
naimuddin94 <developernaimuddin@gmail.com>
2024-05-22 01:04:28 +0600

    create order model

---
+++ src/app/modules/order/order.model.ts
@@ -0,0 +1,30 @@
+import { Schema, model } from 'mongoose';
+import { IOrder } from './order.interface';
+
+const orderSchema = new Schema<IOrder>(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    productId: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Order = model<IOrder>('Order', orderSchema);
+
+export default Order;
