08112514eba64ee9cb9b6aa735df6c84e6de5d1e
naimuddin94 <developernaimuddin@gmail.com>
2024-05-21 19:20:53 +0600

    update product.interface.ts

--- src/app/modules/product/product.interface.ts
+++ src/app/modules/product/product.interface.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { Model } from 'mongoose';
+import { HydratedDocument, Model } from 'mongoose';
 import { z } from 'zod';
 import {
   inventoryValidationSchema,
@@ -11,7 +11,14 @@ export interface IProduct extends z.infer<typeof productValidationSchema> {}
 export interface IVariant extends z.infer<typeof variantValidationSchema> {}
 export interface IInventory extends z.infer<typeof inventoryValidationSchema> {}
 
-export interface IProductMethods {}
+export interface IProductMethods {
+  incrementQuantity(quantity: number): void;
+  decrementQuantity(quantity: number): void;
+}
 
 export interface IProductModel
-  extends Model<IProduct, Record<string, never>, IProductMethods> {}
+  extends Model<IProduct, Record<string, never>, IProductMethods> {
+  isProductNameExists(
+    name: string,
+  ): Promise<HydratedDocument<IProduct, IProductMethods>>;
+}
