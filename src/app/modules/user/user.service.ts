cbd799181be0bd62561ce966161a134c949ac382
naimuddin94 <developernaimuddin@gmail.com>
2024-05-21 10:56:44 +0600

    user login implement

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,7 +2,7 @@ import { ApiError } from '../../../utils';
 import { IUser } from './user.interface';
 import User from './user.model';
 
-// Create a new user
+// Save user to database
 const savedUserToDB = async (userData: IUser) => {
   if (await User.isUserExists(userData.email)) {
     throw new ApiError(400, 'User already exists');
@@ -10,6 +10,45 @@ const savedUserToDB = async (userData: IUser) => {
   return await User.create(userData);
 };
 
+// Fetched all users from the database
+const fetchedUsersToDB = async () => {
+  return await User.find({}).select('-password');
+};
+
+// Fetched single user from the database
+const fetchedSingleUserToDB = async (id: string) => {
+  return await User.findById(id).select('-password');
+};
+
+// Login the user
+const loginUser = async (email: string, password: string) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new ApiError(404, 'User not found');
+  }
+
+  const correctPassword = await user.isPasswordCorrect(password);
+
+  if (!correctPassword) {
+    throw new ApiError(401, 'Invalid credentials');
+  }
+
+  return user;
+};
+
+// Logout the user
+const logoutUser = async (userId: string) => {
+  await User.findByIdAndUpdate(userId, {
+    refreshToken: null,
+    lastLogout: Date.now(),
+  });
+};
+
 export const userService = {
   savedUserToDB,
+  fetchedUsersToDB,
+  fetchedSingleUserToDB,
+  loginUser,
+  logoutUser,
 };
